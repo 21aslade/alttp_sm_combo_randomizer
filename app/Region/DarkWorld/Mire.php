ce886611b78668cfbe7c646dd03f1b4fe405df0c
21aslade <36908617+21aslade@users.noreply.github.com>
2018-08-17 15:55:22 -0600

    Add portal logic to NMG

--- app/Region/DarkWorld/Mire.php
+++ app/Region/DarkWorld/Mire.php
@@ -106,7 +106,7 @@ class Mire extends Region {
 
 		$this->can_enter = function($locations, $items) {
 			return $items->has('RescueZelda')
-				&& (($items->canLiftDarkRocks() && ($items->canFly() || $items->has('PegasusBoots')))
+				&& (($items->canLiftDarkRocks() && ($items->canFly() || $items->has('PegasusBoots'))) || $items->canAccessMiseryMirePortal())
 					|| ($items->has('MoonPearl') && $items->has('PegasusBoots')
 						&& $this->world->getRegion('South Dark World')->canEnter($locations, $items)));
 		};
