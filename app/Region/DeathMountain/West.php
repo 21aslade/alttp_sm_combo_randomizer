f35e78db7edfdbe897cbe8c6309e181740cd26bf
21aslade <36908617+21aslade@users.noreply.github.com>
2018-08-20 13:48:31 -0600

    Add portal logic to OWG

--- app/Region/DeathMountain/West.php
+++ app/Region/DeathMountain/West.php
@@ -116,7 +116,8 @@ class West extends Region {
 		$this->can_enter = function($locations, $items) {
 			return $items->has('RescueZelda')
 				&& ($items->has('PegasusBoots')
-					|| $items->canFly() || ($items->canLiftRocks() && $items->has('Lamp', $this->world->config('item.require.Lamp', 1))));
+					|| $items->canFly() || ($items->canLiftRocks() && $items->has('Lamp', $this->world->config('item.require.Lamp', 1)))
+				   	|| $items->canAccessDeathMountainPortal());
 		};
 
 		return $this;
